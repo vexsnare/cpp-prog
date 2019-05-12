b0d8ba223aa837711bdbef6aa6e1949904eb9beb
Vinay Saini <vinay.saini@flipkart.com>
2019-05-12 23:52:27 +0530

    Implemented median of two sorted array *

--- arrays/binary_search/median_of_two_sorted_array.cpp
+++ arrays/binary_search/median_of_two_sorted_array.cpp
@@ -13,28 +13,50 @@
 #include <queue>
 
 using namespace std;
+#define INF (1<<29)
 
-int findMedian(int ar1[], int n1, int ar2[], int n2) {
-    int i1 = 0, j1 = n1-1;
-    int i2 = 0, j2 = n2-1;
-    while (true) {
-        int m1 = (i1 + j1)/2;
-        int m2 = (i2 + j2)/2;
-
+// Watch Tushar roy video if forgot the logic - We do binary search only on first array & get relative partition point on second array
+double findMedian(int ar1[], int n1, int ar2[], int n2) {
+    if(n1 > n2) {
+        return findMedian(ar2, n2, ar1, n1);
     }
-    int result = 0;
-    return result;
+    int lo = 0;
+    int hi = n1;
+    while (lo <= hi) {
+        int p1 = (lo+hi)/2;
+        int p2 = (n1 + n2 + 1)/2 - p1;
+
+        int ar1Left = p1 == 0 ? -INF : ar1[p1-1];
+        int ar1Right = p1 == n1 ? INF : ar1[p1];
+
+        int ar2Left = p2 == 0 ? -INF : ar2[p2-1];
+        int ar2Right = p2 == n2 ? INF : ar2[p2];
+
+        if(ar1Left <= ar2Right && ar2Left <= ar1Right) {
+            if((n1+n2)%2 == 0) {
+                return (max(ar1Left, ar2Left) + min(ar1Right, ar2Right))/2.0;
+            }
+            return max(ar1Left, ar2Left);
+        } else if(ar1Left > ar2Right) {
+            hi = p1-1;
+        } else lo = p1 + 1;
+    }
 }
 
 int main() {
     int n1, n2;
-    cin >> n1, n2;
+    cin >> n1 >> n2;
     int ar1[n1];
     int ar2[n2];
+    for (int i = 0; i < n1; ++i) {
+        cin >> ar1[i];
+    }
+    for (int i = 0; i < n2; ++i) {
+        cin >> ar2[i];
+    }
     sort(ar1, + ar1 + n1);
     sort(ar2, + ar2 + n2);
-    int ans = findMedian(ar1, n1, ar2, n2) {
-        cout << ans << "\n";
-    }
+    double ans = findMedian(ar1, n1, ar2, n2);
+    cout << ans;
     return 0;
 }
