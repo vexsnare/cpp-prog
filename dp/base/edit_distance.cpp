8c22dfd4a0c72dba06bdd237b4ec02c5d8e0a0f4
Vinay Saini <vinay.saini@flipkart.com>
2019-07-25 15:27:34 +0530

    implemented edit distance

--- dp/base/edit_distance.cpp
+++ dp/base/edit_distance.cpp
@@ -21,6 +21,24 @@ using namespace std;
 #define INF (1<<29)
 #define min(a, b) ((a)<(b)?(a):(b))
 #define max(a, b) ((a)>(b)?(a):(b))
+#define MAX 100
+
+string str1;
+string str2;
+int memo[MAX][MAX];
+
+int solve(int n1, int n2) {
+    if(n1 < 0 && n2 < 0) {
+        return 0;
+    }
+    if(n1 < 0) return 1+n2;
+    if(n2 < 0) return 1+n1;
+    if(memo[n1][n2] != -1) return memo[n1][n2];
+    if(str1[n1] == str2[n2]) return solve(n1-1, n2-1);
+    int ret = 1 + min(min(solve(n1, n2-1), solve(n1-1, n2)), solve(n1-1, n2-1));
+    memo[n1][n2] = ret;
+    return ret;
+}
 
 /**
  * https://www.techiedelight.com/levenshtein-distance-edit-distance-problem
@@ -29,9 +47,28 @@ using namespace std;
  * @return
  */
 int findMinEditDistance(string &s1, string &s2) {
-    
+    str1 = s1;
+    str2 = s2;
+    int n1 = s1.length();
+    int n2 = s2.length();
+    memset(memo, -1, MAX*MAX);
+    return solve(n1-1, n2-1);
 }
 
 int main() {
+
+    string s1 = "kitten";
+    string s2 = "sitting";
+    assert(findMinEditDistance(s1, s2) == 3);
+
+    s1 = "kitten";
+    s2 = "kitten";
+    assert(findMinEditDistance(s1, s2) == 0);
+
+
+    s1 = "kitten";
+    s2 = "";
+    assert(findMinEditDistance(s1, s2) == 6);
+
     return 0;
 }
